f52f8a269de9a123512edae242b311b935c4a5fc
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2022-11-18 21:16:42 -0500

    added exit option

--- sagredov_jeserna.asm
+++ sagredov_jeserna.asm
@@ -10,21 +10,26 @@ imprimir macro str
 endm
 
 leer macro destino
-         call prc_leer
-         mov  destino, al
+         call     prc_leer
+         mov      destino, al
+         imprimir newline
 endm
 
 .model small
 .stack 200h
 .data
+    newline             db 10,13,'$'
     msgcontraseniaa     db 'Ingrese la contraseña: ',10,13,'$'
     nummaxintentos      db 3
     msgerrorcontrasenia db 'La contraseña ingresada fue incorrecta, presione una tecla para salir',10,13,'$'
     msgmenu             db '1. Suma y diferencia de dos numeros entre -16384 a 16383',10,13
                         db '2. Multiplicacion y division de dos numeros entre -128 a 127',10,13
                         db '3. Operaciones logicas, NOT, AND, OR, XOR de dos numeros entre 00 a FF',10,13
-                        db '4. Primeros 16 numeros de la serie alternada',10,13,'$'
+                        db '4. Primeros 16 numeros de la serie alternada',10,13
+                        db '5. Salir',10,13,'$'
     opcionescogida      db 0
+    waitvar             db 0
+    msgsalida           db 'Saliendo...',10,13,'$'
     flgnegativo         db 0
     maxdivisordecmiles  dw 10000
     maxdivisorcents     db 100
@@ -32,26 +37,31 @@ endm
 
 .code
 main proc near
-                 cargar_datos
-    autenticado: 
-                 imprimir     msgmenu
-                 leer         opcionescogida
-                 jmp          autenticado
+                    cargar_datos
+    lblautenticando:
+                    imprimir     msgmenu
+                    leer         opcionescogida
+                    cmp          opcionescogida,'5'
+                    je           salir
+                    jmp          lblautenticando
+   
 main endp
 prc_imprimir PROC
-                 mov          ah, 09h
-                 int          21h
-                 ret
+                    mov          ah, 09h
+                    int          21h
+                    ret
 prc_imprimir ENDP
     
 prc_leer PROC
-                 mov          ah, 1
-                 int          21h
-                 ret
+                    mov          ah, 1
+                    int          21h
+                    ret
 prc_leer ENDP
 
 salir PROC
-                 mov          ah,4ch
-                 int          21h
+                    imprimir     msgsalida
+                    leer         waitvar
+                    mov          ah,4ch
+                    int          21h
 salir ENDP
 end main
