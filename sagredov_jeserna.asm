0519d403502e96c22ab765d602624d41a5eeec1a
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2022-11-18 21:08:29 -0500

    added menu loop

--- sagredov_jeserna.asm
+++ sagredov_jeserna.asm
@@ -1,4 +1,9 @@
 ;MACROS
+cargar_datos MACRO
+                 mov ax, @data
+                 mov ds, ax
+ENDM
+
 imprimir macro str
              mov  dx, offset str
              call prc_imprimir
@@ -8,6 +13,7 @@ leer macro destino
          call prc_leer
          mov  destino, al
 endm
+
 .model small
 .stack 200h
 .data
@@ -18,34 +24,34 @@ endm
                         db '2. Multiplicacion y division de dos numeros entre -128 a 127',10,13
                         db '3. Operaciones logicas, NOT, AND, OR, XOR de dos numeros entre 00 a FF',10,13
                         db '4. Primeros 16 numeros de la serie alternada',10,13,'$'
+    opcionescogida      db 0
     flgnegativo         db 0
     maxdivisordecmiles  dw 10000
     maxdivisorcents     db 100
 
+
 .code
 main proc near
-                 mov  ax, @data
-                 mov  ds, ax
-                 mov  ah, 09
-                 mov  dx, offset msgmenu
-                 int  21h                   ;
-                 call salir
-                 
+                 cargar_datos
+    autenticado: 
+                 imprimir     msgmenu
+                 leer         opcionescogida
+                 jmp          autenticado
 main endp
 prc_imprimir PROC
-                 mov  ah, 09h
-                 int  21h
+                 mov          ah, 09h
+                 int          21h
                  ret
 prc_imprimir ENDP
     
 prc_leer PROC
-                 mov  ah, 1
-                 int  21h
+                 mov          ah, 1
+                 int          21h
                  ret
 prc_leer ENDP
 
 salir PROC
-                 mov  ah,4ch
-                 int  21h
+                 mov          ah,4ch
+                 int          21h
 salir ENDP
 end main
