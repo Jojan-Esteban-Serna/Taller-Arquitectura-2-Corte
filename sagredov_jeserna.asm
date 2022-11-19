9067d2d2e1cbbd8e49f202f090c1f95f73a9a7e9
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2022-11-18 23:23:46 -0500

    Adde authentication

--- sagredov_jeserna.asm
+++ sagredov_jeserna.asm
@@ -24,30 +24,41 @@ endm
 .model small
 .stack 200h
 .data
-    contrasenia          db 'ggezpz'
-    newline              db 10,13,'$'
-    msgcontrasenia       db 'Ingrese la contraseña: ',10,13,'$'
-    contraseniaingresada db 'ci123456e','$'                                                                      ;(c)cantidad de elementos permitidos,(i) elementos ingresados,(e)enter al ingresar
-    nummaxintentos       db 3
-    msgerrorcontrasenia  db 'La contraseña ingresada fue incorrecta, presione una tecla para salir',10,13,'$'
-    msgmenu              db '1. Suma y diferencia de dos numeros entre -16384 a 16383',10,13
-                         db '2. Multiplicacion y division de dos numeros entre -128 a 127',10,13
-                         db '3. Operaciones logicas, NOT, AND, OR, XOR de dos numeros entre 00 a FF',10,13
-                         db '4. Primeros 16 numeros de la serie alternada',10,13
-                         db '5. Salir',10,13,'$'
-    opcionescogida       db 0,10,13,'$'
-    waitvar              db 0
-    msgsalida            db 'Saliendo...',10,13,'$'
-    msgprestecla         db 'Presione una tecla para continuar...',10,13,'$'
-    flgnegativo          db 0
-    maxdivisordecmiles   dw 10000
-    maxdivisorcents      db 100
+    contrasenia              db 'ggezpz'
+    newline                  db 10,13,'$'
+    msgcontrasenia           db 'Ingrese la contraseña: ',10,13,'$'
+    msgcontraseniaincorrecta db 'Las tres contraseñas ingresadas fueron incorrectas',10,13,'$'
+    contraseniaingresada     db 'ci123456e','$'
+    nummaxintentos           dw 3
+    msgerrorcontrasenia      db 'La contraseña ingresada fue incorrecta, presione una tecla para salir',10,13,'$'
+    msgmenu                  db '1. Suma y diferencia de dos numeros entre -16384 a 16383',10,13
+                             db '2. Multiplicacion y division de dos numeros entre -128 a 127',10,13
+                             db '3. Operaciones logicas, NOT, AND, OR, XOR de dos numeros entre 00 a FF',10,13
+                             db '4. Primeros 16 numeros de la serie alternada',10,13
+                             db '5. Salir',10,13,'$'
+    opcionescogida           db 0,10,13,'$'
+    waitvar                  db 0
+    msgsalida                db 'Saliendo...',10,13,'$'
+    msgprestecla             db 'Presione una tecla para continuar...',10,13,'$'
+    flgnegativo              db 0
+    maxdivisordecmiles       dw 10000
+    maxdivisorcents          db 100
 
 
 .code
 main proc near
                          cargar_datos
+                         mov          cx, nummaxintentos
                          
+    autenticacion:       
+                         imprimir     msgcontrasenia
+                         leer_string  contraseniaingresada, 7     ; en realidad lee 6 caracteres y el enter
+                         loop         autenticacion
+                         imprimir     msgcontraseniaincorrecta
+                         imprimir     msgsalida
+                         imprimir     msgprestecla
+                         leer_char    waitvar
+                         call         prc_salir
     autenticado:         
                          call         prc_limpiar_pantalla
                          imprimir     msgmenu
